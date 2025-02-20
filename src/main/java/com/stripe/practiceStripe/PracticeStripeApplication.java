c46bf0e7b89e39a99898013c84f4568f402175c7
Martin Bjervig <martinbjervig@gmail.com>
2025-02-20 20:22:59 +0100

    added req files to repo

---
+++ src/main/java/com/stripe/practiceStripe/PracticeStripeApplication.java
@@ -0,0 +1,13 @@
+package com.stripe.practiceStripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeStripeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stripe/practiceStripe/PracticeStripeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stripe.practiceStripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeStripeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
